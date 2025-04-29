3517dbb787340e2542d392b7dc4c35864345248d
blue&blue <blueandblue@users.noreply.github.com>
2025-04-29 11:56:56 +0200

    Add files via upload

---
+++ export.php
@@ -0,0 +1,41 @@
+﻿<?php
+include "db.php";
+
+if (isset($_POST["exportType"])) {
+    $type = $_POST["exportType"];
+    $table = $_POST["table"];
+    $output = "";
+
+    if ($type == "csv") {
+        $filename = "export/$table.csv";
+        $query = "SELECT * FROM `$table`";
+        $result = $conn->query($query);
+
+        $f = fopen($filename, "w");
+        $fields = array_keys($result->fetch_assoc());
+        fputcsv($f, $fields);
+
+        while ($row = $result->fetch_assoc()) {
+            fputcsv($f, $row);
+        }
+        fclose($f);
+
+        echo "Esportato in CSV: <a href='$filename' download>Scarica</a>";
+    } elseif ($type == "sql") {
+        $filename = "export/$table.sql";
+        $query = "SHOW CREATE TABLE `$table`";
+        $result = $conn->query($query);
+        $row = $result->fetch_assoc();
+        $output .= $row["Create Table"] . ";\n\n";
+
+        $query = "SELECT * FROM `$table`";
+        $result = $conn->query($query);
+        while ($row = $result->fetch_assoc()) {
+            $output .= "INSERT INTO `$table` VALUES (" . implode(",", array_map(fn($v) => "'$v'", $row)) . ");\n";
+        }
+
+        file_put_contents($filename, $output);
+        echo "Esportato in SQL: <a href='$filename' download>Scarica</a>";
+    }
+}
+?>
